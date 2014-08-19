183ac1889f369743a86a8a2ff6b61486acda614e
Maks Naumov <maksqwe1@ukr.net>
2014-08-19 14:59:14 -0700

    Fix width and height args for _cairo_xcb_connection_copy_area()
    Found via static analysis.
    
    Signed-off-by: Maks Naumov <maksqwe1@ukr.net>
    Reviewed-by:  Uli Schlachter <psychon@znc.in>

--- src/cairo-xcb-surface-core.c
+++ src/cairo-xcb-surface-core.c
@@ -573,7 +573,7 @@ _cairo_xcb_surface_core_copy_boxes (cairo_xcb_surface_t		*dst,
 						 src->x0 + x1,
 						 src->y0 + y1,
 						 x1, y1,
-						 x2 - x2, y2 - x2);
+						 x2 - x1, y2 - y1);
 	    }
 	}
     }

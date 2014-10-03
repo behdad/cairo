f57c0fd4656ef4fdbe13dbc69aa1ce4e130c37ce
Bryce Harrington <bryce@osg.samsung.com>
2014-10-03 12:20:13 -0700

    Declare as private the new cairo_lines_compare_at_y symbol
    Fixes a make check error.
    
      Checking that .libs/libcairo.so has the same symbol list as cairo.def
      126a127
      > cairo_lines_compare_at_y
    
    Fixes:  https://bugs.freedesktop.org/show_bug.cgi?id=84638
    Signed-off-by: Bryce Harrington <b.harrington@samsung.com>

--- src/cairo-line-private.h
+++ src/cairo-line-private.h
@@ -41,9 +41,10 @@
 
 CAIRO_BEGIN_DECLS
 
-int cairo_lines_compare_at_y(const cairo_line_t *a,
-			     const cairo_line_t *b,
-			     int y);
+cairo_private int
+cairo_lines_compare_at_y(const cairo_line_t *a,
+			 const cairo_line_t *b,
+			 int y);
 
 CAIRO_END_DECLS
 

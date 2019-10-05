d0dccb92c520556aaa02bd1fdf8f2922cef00292
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2019-10-05 14:07:28 +0200

    image compositor: Remove the right glyph from pixman's cache
    We need to use the index including the phase. Otherwise we leave glyphs
    in the cache that cause problems later as indices are reused.

--- src/cairo-image-compositor.c
+++ src/cairo-image-compositor.c
@@ -841,7 +841,7 @@ _cairo_image_scaled_glyph_fini (cairo_scaled_font_t *scaled_font,
     if (global_glyph_cache) {
 	pixman_glyph_cache_remove (
 	    global_glyph_cache, scaled_font,
-	    (void *)_cairo_scaled_glyph_index (scaled_glyph));
+	    (void *)scaled_glyph->hash_entry.hash);
     }
 
     CAIRO_MUTEX_UNLOCK (_cairo_glyph_cache_mutex);

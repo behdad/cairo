4c6b604bd512c2619c69c7f2afb9481ab756bf85
Uli Schlachter <psychon@znc.in>
2021-07-24 14:42:26 +0200

    svg: Fix create-for-stream test case
    Commit 2fbd53 added another test to create-for-stream that failed for
    cairo-pdf. Manual testing shows that this test also fails for cairo-svg.
    However, this was not noticed because even before this addition to the
    test, create-for-stream already failed for cairo-svg. Since the
    introduction on CI was done based on "let's ignore all current
    failures", this hid the error.
    
    This commit applies the trivial fix for cairo-svg to make this test
    pass. It is basically the same error that cairo-pdf had.
    
    Additionally, this removes the hack to ignore create-for-stream failures
    in CI since it is now no longer necessary.
    
    Thanks to Knut Peterson for reporting this [0].
    
    [0]:
    https://lists.cairographics.org/archives/cairo/2021-July/029291.html
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- test/cairo-test-runner.c
+++ test/cairo-test-runner.c
@@ -942,18 +942,6 @@ main (int argc, char **argv)
 
 	if (ctx.test->preamble != NULL) {
 	    status = _cairo_test_runner_preamble (&runner, &ctx);
-	    if (getenv ("CAIRO_TEST_UGLY_HACK_TO_IGNORE_CREATE_FOR_STREAM") && strcmp (ctx.test_name, "create-for-stream") == 0) {
-		if (status == CAIRO_TEST_FAILURE) {
-		    cairo_test_log (&ctx, "Turning FAIL into XFAIL due to env\n");
-		    fprintf (stderr, "Turning FAIL into XFAIL due to env\n");
-		    runner.num_ignored_via_env++;
-		    status = CAIRO_TEST_XFAILURE;
-		} else {
-		    fprintf (stderr, "Test was expected to fail due to an environment variable, but did not!\n");
-		    fprintf (stderr, "Please remove the hack to ignore create-for-stream failures.\n");
-		    status = CAIRO_TEST_ERROR;
-		}
-	    }
 	    switch (status) {
 	    case CAIRO_TEST_SUCCESS:
 		in_preamble = TRUE;
